800e81fd029b30e2364a7e076b1b81a0d75d194d
Shivank98 <shivankgautam98@gmail.com>
2017-10-17 11:50:56 +0530

    second commit

--- ironmap/home/models.py
+++ ironmap/home/models.py
@@ -9,6 +9,13 @@ class Input(models.Model):
     User = models.CharField(max_length=150)
     Question = models.TextField()
 
+    def __str__(self):
+        return self.User, self.Question
+
+
 class TestCase(models.Model):
     InputS = models.ForeignKey('Input')
     OutputS = models.ForeignKey('Input')
+
+    def __str__(self):
+        return self.OutputS
